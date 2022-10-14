e20277c16e6fe46c856bd6f9ec5de763c949c340
Chenna Chandrika <chennachandrikapadmashali@gmail.com>
2022-10-14 20:36:31 +0530

    saved jobs

--- src/components/AppliedJobs/AppliedJobs.js
+++ src/components/AppliedJobs/AppliedJobs.js
@@ -20,22 +20,22 @@ const appliedJobsData = [
     jobPosted: "2 Days Ago",
   },
   {
-    role: "Angular Developer",
-    company: "Maximl Labs Private Limited",
-    experience: "1-3 Yrs",
-    salary: "3,00,000 - 5,00,000 PA",
-    location: "Chennai",
+    role: "React Developer",
+    company: "Codemonk Elvago Private Limited",
+    experience: "2-3 Yrs",
+    salary: "5,00,000 - 8,00,000 PA",
+    location: "Bangalore",
     responsibilities:
-      "Deep knowledge on Angular 2+ practices and commonly used modules based on extensive work",
+      "Deep knowledge on React practices and commonly used modules based on extensive work",
     requiredSkills: [
-      "Angular",
-      "Angular 2+",
+      "React",
+      "React Native",
       "CSS",
-      "Angular/software developer",
-      "Angular/Developer",
+      "React/software developer",
+      "React/Developer",
     ],
     isHotJob: true,
-    jobPosted: "2 Days Ago",
+    jobPosted: "5 Days Ago",
   },
 ];
 
@@ -49,48 +49,90 @@ const AppliedJobs = () => {
             <h3>{item.role}</h3>
             <p>{item.company}</p>
             <div className="information-container">
-                <div className="job-3-info">
+              <div className="job-3-info">
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
                 >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M7 5V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h4a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h4zM4 15v4h16v-4H4zm7-4v2h2v-2h-2zM9 3v2h6V3H9z" />
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M7 5V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3h4a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h4zM4 15v4h16v-4H4zm7-4v2h2v-2h-2zM9 3v2h6V3H9z" />
                 </svg>
                 <p className="job-exp">{item.experience}</p>
-                </div>
-                <div className="job-3-info">
+              </div>
+              <div className="job-3-info">
                 <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
                 >
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M17 16h2V4H9v2h8v10zm0 2v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3zM7 16v2h2v1h2v-1h.5a2.5 2.5 0 1 0 0-5h-3a.5.5 0 1 1 0-1H13v-2h-2V9H9v1h-.5a2.5 2.5 0 1 0 0 5h3a.5.5 0 1 1 0 1H7z" />
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M17 16h2V4H9v2h8v10zm0 2v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3zM7 16v2h2v1h2v-1h.5a2.5 2.5 0 1 0 0-5h-3a.5.5 0 1 1 0-1H13v-2h-2V9H9v1h-.5a2.5 2.5 0 1 0 0 5h3a.5.5 0 1 1 0 1H7z" />
                 </svg>
                 <p className="job-exp">{item.salary}</p>
-                </div>
-                <div className="job-3-info">
+              </div>
+              <div className="job-3-info">
                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M18.364 17.364L12 23.728l-6.364-6.364a9 9 0 1 1 12.728 0zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                </svg>
+                <p className="job-exp">{item.location}</p>
+              </div>
+            </div>
+            <div className="job-3-info">
+              <div style={{ width: "24px" }}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z" />
+                </svg>
+              </div>
+              <p className="job-exp">{item.responsibilities}</p>
+            </div>
+            <div className="information-container">
+              {item.requiredSkills.map((skill) => (
+                <p style={{marginRight:"10px"}}>{skill}</p>
+              ))}
+            </div>
+            <div className="information-container">
+              {item.isHotJob && (
+                <div className="job-3-info">
+                  <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     width="24"
                     height="24"
-                >
+                  >
                     <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M18.364 17.364L12 23.728l-6.364-6.364a9 9 0 1 1 12.728 0zM12 13a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                    <path d="M12 23a7.5 7.5 0 0 1-5.138-12.963C8.204 8.774 11.5 6.5 11 1.5c6 4 9 8 3 14 1 0 2.5 0 5-2.47.27.773.5 1.604.5 2.47A7.5 7.5 0 0 1 12 23z" />
+                  </svg>
+                  <p className="job-exp">{item.isHotJob && "Hot Job"}</p>
+                </div>
+              )}
+              <div className="job-3-info">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0H24V24H0z" />
+                  <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12h2c0 4.418 3.582 8 8 8s8-3.582 8-8-3.582-8-8-8C9.25 4 6.824 5.387 5.385 7.5H8v2H2v-6h2V6c1.824-2.43 4.729-4 8-4zm1 5v4.585l3.243 3.243-1.415 1.415L11 12.413V7h2z" />
                 </svg>
-                <p className="job-exp">{item.location}</p>
-                </div>
+                <p className="job-exp">{item.jobPosted}</p>
+              </div>
             </div>
-            <div className="job-3-info">
-                <div style={{width:"24px"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zM6 7v2h8V7H6zm0 4v2h8v-2H6zm0 4v2h5v-2H6z"/></svg>
-            </div><p className="job-exp">{item.responsibilities}</p>
-                </div>
           </div>
         ))}
       </div>
